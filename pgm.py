411c0ec9aefe1ee21a0d2495d81a2c40d7ac47d8
RakshithaNekar123 <160992628+RakshithaNekar123@users.noreply.github.com>
2024-12-17 10:33:56 +0530

    Add files via upload

---
+++ pgm.py
@@ -0,0 +1,696 @@
+# s=input('enter the string')
+# if len(s)==10:
+#     for i in s:
+#         if'a'<=i<='z':
+#             print(i)
+# elif len(s)<10:
+#     for i in s:
+#         if 'A'<=i<='Z':
+#             print(i)
+# elif len(s)>10:
+#     for i in s:
+#         if not('a'<=i<='z' or 'A'<=i<='Z' ):
+#             print(i)
+
+# s=input('enter the string')
+# u=''
+# l=''
+# sp=''
+# for in in s;
+#     if i.isupper():
+#         v+=input
+#     elif i.islower():
+#         l+=i
+#     else:
+#         sp+=i 
+
+# l=eval(input())
+# d ={}
+# for i in l:
+#      if type(l)==str:
+#        if i[0].isupper():
+#         d[i]=i[0]
+#      else:
+#        d[i]=ord(i[0])
+# print(d)     
+
+# n=int(input("enter the number"))
+# st=""
+# i=0
+# while i<=n:
+#     st+=str(i)
+#     i+=1
+# print(st)    
+        
+
+# s=eval(input("enter the scores"))
+# s.remove(max(s))
+# print(max(s)) 
+
+# s=eval(input("enter the score"))
+# max=0
+# for i in s:
+#     if i>max:
+#         max=i
+
+# runner_up=0 
+# for j in s:
+#     if j<max and j>runner_up:
+#         runner_up=j
+# print(runner_up)              
+
+
+# n=int (input('enter the number'))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(str(i)+str(j),end='\t')
+#     print()
+
+
+# n=int(input('enter the number'))
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if i==j:
+#             print('@')  
+#         elif i>j:
+#             print(' #' , end=' ')
+#     print()        
+
+
+# n=int(input('enter the number'))
+# for i in range (1,n+1):
+#     for j in range (1,n+1):
+#         if i>j:
+#             print('$')
+#         else:
+#             print(' !',end=' ')    
+#     print()    
+
+
+
+# n=int(input('enter the number'))
+# for i in range (1,n+1):
+#     for j in range (1,n+1):
+#         if i==j:
+#             print('#' , end=' ')
+#         elif i<j:
+#             print('! ',end=' ') 
+#         else:
+#             print(' ',end=' ')          
+    
+#     print()
+
+
+
+
+# n=int(input('enter the number'))
+# for i in range (1,n+1):
+#     for j in range (1,n+1):
+#         if i+j==n+1:
+#             print('*' , end=' ')
+#         else:
+#             print(' ', end=' ')
+#     print()             
+
+
+# n=int(input('enter the number'))
+# for i in range (1,n+1):
+#     for j in range (1,n+1):
+#         if i+j<n+1:
+#             print('#' , end=' ')
+#         else:
+#             print(' ', end=' ')
+#     print()             
+    
+
+
+# n=int(input('enter the number'))
+# for i in range (1,n+1):
+#     for j in range (1,n+1):
+#         if i+j<n+1:
+#             print('#' , end=' ')
+#         else:
+#             print(' ', end=' ')
+#     print()             
+
+
+# n=int(input('enter the number'))
+# for i in range (1,n+1):
+#     for j in range (1,n+1):
+#         if i+j>n+1:
+#             print('@' , end=' ')
+#         else:
+#             print(' ', end=' ')
+#     print()             
+    
+
+# n=int(input('enter the number'))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#          if i+j==n+2: 
+#                 print('$' , end=' ')
+#          elif i+j==n+3:
+#                 print('@' , end=' ')
+#          elif i+j==n+4:
+#                 print('#' , end=' ')
+#          elif i+j==n+5:
+#                 print('&' , end=' ')
+#          else:
+#             print(' ' , end=' ')
+#     print()
+
+
+# n=int(input('enter the number'))
+# for  i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if i==n:
+#             print('*' , end=' ')
+#         elif i+j==n+1 and i>n/2:
+#             print('*' ,end=' ')
+#         elif i==j and i>n/2:
+#             print('*' , end=' ')        
+#         else:
+#             print(' ' , end=' ')
+#     print()        
+
+
+# n=int (input('enter the number'))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(str(i)+str(j),end='\t')
+#     print()
+
+
+# n=int(input('Enter the number'))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if i==1 or j==1 or i==n or j==n:
+#             print('*' , end=' ')
+#         else:
+#             print(' ', end=' ')
+#     print()            
+
+# n=int(input('Enter the number'))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if i+j==n+1:
+#             print('1' , end=' ')
+#         else:
+#             print('0 ', end=' ')
+#     print()            
+
+
+# n=int(input('Enter the number'))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if i==j:
+#             print(' ' , end=' ')
+#         elif j>i:    
+#             print('@' ,end=' ')
+        
+#         else:
+#             print('$ ', end=' ')
+#     print()      
+
+
+# n=int(input('Enter the number'))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if i==n//2+1 or j==n//2+1 :
+#             print('* ' , end=' ')
+#         else:
+#             print(' ', end=' ')
+#     print()            
+      
+
+# n=int(input('Enter the number'))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if i in(1,n) or j in (1,n) or i+j==n+1 or i==j:
+#             print('*' , end=' ')
+#         else:
+#             print(' ', end=' ')
+    # print()     
+       
+# c=2
+# n=int(input('enter the number'))
+# for i in range (1,n+1):
+#     for j in range (1,n+1):
+#         if i>j:
+#             print(c, end=' ')
+#             c+=1
+           
+#     print()    
+
+# char= 'A'
+# n=int(input('enter the number'))
+# for i in range (1,n+1):
+#     for j in range (1,n+1):
+#         if i>=j:
+#             print(char,end=' ')
+#             char=chr(ord(char)+1)
+        
+#     print()
+
+# def generator(x):
+#     yield x+20
+#     yield x+30
+# print(list(generator(10)))
+
+
+
+# def fib(n):
+#     a=0
+#     b=1
+#     while a<=n:
+#         print(a)
+#         next=a+b
+#         a=b
+#         b=next
+# fib(20)        
+
+
+
+# n=int(input('enter the number'))
+# for i in range (1,n+1):
+#     for j in range (1,n+1):
+#         if i in (1,n) or j in (1,n) or i+j==n+1 or 1==j:
+#             print('*' , end=' ')
+#         else:
+#             print(' ', end=' ')
+#     print()                            
+
+
+# n=int(input('enter the number'))
+# for i in range (1,n+1):
+#     for j in range (1,n+1):
+#         if i in (1,n) or j in (1,n) or i+j==n+1 or i==j:
+#             print('*' , end=' ')
+#         else:
+#             print(' ', end=' ')
+#     print()                            
+
+
+# def is_square(n):
+#     for i in n:
+#         if type(i)==int and 0<=i<=9:
+#             yield i**2
+# print(list(is_square(['hai','bye',[5,7],7 , 9 , 5])))      
+
+# def is_fib(n):
+#     if n<0:
+#         return 'number shouldbe greater than C'
+#     a=0
+#     b=1
+#     for i in range(n):
+#         print(a)
+#         c=a+b
+#         a=b
+#         b=c
+# (is_fib(3))        
+
+# def is_fib(n):
+#     if n<0:
+#         return 'it is not a fib'
+#     a=0
+#     b=1
+#     while True:
+#         if a==n:
+#             return 'it is a fib'
+#         c=a+b
+#         a=b
+#         b=c
+#         if a>n:
+#             return 'it is not a fib'
+# print(is_fib(444))      
+
+
+# def display(n):
+#     num={1:'one' , 2:'two' , 3:'three' , 4:'four', 5:'five' , 6:'six' , 7:'seven' , 8:'eight' , 9:'nine',0:'zero'}
+#         sum=0
+#             for i in str(n):
+#                 sum+=int(i)
+#              out=' '
+#     for j in str(sum):
+#         out+=num[int(i)]
+#     return out   
+# print(display(24))             
+
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        self.prev=None
+class doublylinkedlist:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+
+    def insertatbeginning(self,new_data):
+        new_node=Node(new_data)
+        if self.head==None:
+            self.head=new_node
+            self.tail=new_node
+            return
+        
+        new_node.next=self.head
+        self.head.prev=new_node
+        self.head=new_node
+
+    def deleteatbegining(self):
+        if self.head==None:
+            print('empty')
+            return
+        if self.head==self.tail:
+            self.head=None
+            self.tail=None
+            return
+        self.head=self.head.next
+        self.head.prev=None   
+
+    def deleteatend(self,new_data):
+        if self.head==None:
+            self.head=self.head
+             
+
+    def insertatend(self,new_data):
+        new_node=Node(new_data)
+        if self.head==None:
+            self.head=new_node
+            self.tail=new_node
+            return
+        self.tail.next=new_node
+        new_node.prev=self.tail
+        self.tail=new_node
+    def printlist(self):
+        temp=self.head
+        if temp==None:
+            print('list is empty')
+            return
+        while temp:
+            print(temp.data,end='')   
+            temp=temp.next
+        print()
+
+d1=doublylinkedlist()
+d1.insertatbeginning(10)
+d1.insertatbeginning(20)
+d1.insertatbeginning(30)
+d1.insertatbeginning(40)
+d1.printlist()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+               
